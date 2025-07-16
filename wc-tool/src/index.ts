e18ab6bd3b3223b3488f50eda81547d29918faec
EndersbyD <david.endersby1@gmail.com>
2025-07-16 18:15:29 +0100

    step 3 of coding challenge complete

--- wc-tool/src/index.ts
+++ wc-tool/src/index.ts
@@ -9,6 +9,7 @@ program
   .version('1.0.0')
   .option('-c', 'count of bytes in a file')
   .option('-l', 'count number of lines in a file')
+  .option('-w', 'count the number of words in a file')
   .argument('<path>')
   .action((path, options) => {
     if (options.c) {
@@ -19,6 +20,13 @@ program
       const data = readFileSync(path, 'utf8');
       const lineCount = data.split('\n').length;
       console.log(`${lineCount} ${path}`);
+    }
+    if (options.w) {
+      const data = readFileSync(path, 'utf8');
+      // Match words using regex: sequences of non-whitespace characters
+      const words = data.match(/\S+/g);
+      const wordCount = words ? words.length : 0;
+      console.log(`${wordCount} ${path}`);
     }
   });
 
