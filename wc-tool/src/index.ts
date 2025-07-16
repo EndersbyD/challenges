1295d5fc8eead0b9bbcd8271129ad5160df9d99f
EndersbyD <david.endersby1@gmail.com>
2025-07-16 18:30:48 +0100

    step 4 & 5 of coding challenge complete

--- wc-tool/src/index.ts
+++ wc-tool/src/index.ts
@@ -10,23 +10,30 @@ program
   .option('-c', 'count of bytes in a file')
   .option('-l', 'count number of lines in a file')
   .option('-w', 'count the number of words in a file')
+  .option('-m', 'count the number of characters in a file')
   .argument('<path>')
   .action((path, options) => {
+    const stats = statSync(path);
+    const data = readFileSync(path, 'utf8');
+    const lineCount = data === '' ? 0 : data.split('\n').length - (data.endsWith('\n') ? 1 : 0);
+    const words = data.match(/\S+/g);
+    const wordCount = words ? words.length : 0;
+
+    if (Object.keys(options).length === 0) {
+      console.log(`${lineCount} ${wordCount} ${stats.size} ${path}`);
+    }
     if (options.c) {
-      const stats = statSync(path);
       console.log(`${stats.size} ${path}` );
     }
     if (options.l) {
-      const data = readFileSync(path, 'utf8');
-      const lineCount = data.split('\n').length;
       console.log(`${lineCount} ${path}`);
     }
-    if (options.w) {
-      const data = readFileSync(path, 'utf8');
-      // Match words using regex: sequences of non-whitespace characters
-      const words = data.match(/\S+/g);
-      const wordCount = words ? words.length : 0;
+    if (options.w) {      
       console.log(`${wordCount} ${path}`);
+    }
+    if (options.m) {
+      const charCount = data.length;
+      console.log(`${charCount} ${path}`);
     }
   });
 
